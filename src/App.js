417188b224228bc235d8b1e22eebe5ea92d39112
Pranay Pise <pranaypise59@gmail.com>
2022-11-29 00:49:32 +0530

    creating the show page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 import './App.css';
 import RenderMainScreen from './components/RenderMainScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={RenderMainScreen(<Home />)} />
         <Route exact path='/starred' element={RenderMainScreen(<Starred />)} />
+        <Route exact path='/show/:id' element={<Show />} />
         <Route path='*' element={<div>Not Found</div>}></Route>
       </Routes>
     </Router>
---
+++ src/pages/Show.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+      setShow(results);
+      setIsLoading(false)
+    }).catch(err =>{setError(err.message);setIsLoading(false)});
+  }, [id]);
+
+  console.log('show', show);
+
+  return <div>this is show page</div>;
+};
+
+export default Show;
