dcc01e540e0f07ae12c63c1f2f4c04f5c9318ef7
Pranay Pise <pranaypise59@gmail.com>
2022-11-29 15:29:46 +0530

    finished Starred Page data loading

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import RenderMainScreen from '../components/RenderMainScreen';
+import ShowGrid from '../components/shows/ShowGrid';
+import { apiGet } from '../misc/config';
+import { useShows } from '../misc/custom-hooks'
 
 const Starred = () => {
+  const [starred] = useShows();
+
+  const [shows, setShows]= useState(null)
+  const [isLoading, setIsLoading]= useState(true)
+  const [error, setError]= useState(null)
+
+
+  useEffect(() => {
+    if(starred && starred.length >0){
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+      Promise.all(promises).then(apiData=>apiData.map(show=>({show}))).then(results=>{
+        setShows(results);
+        setIsLoading(false)
+      }).catch(err=>{setError(err.message);setIsLoading(false)})
+    }
+  
+  }, [starred])
+  
   return (
-    <div>Starred</div>
+    <>
+    {isLoading && <div>The Data is Still Loading</div>}
+    {error && <div>Error Occured: {error}</div>}
+    {!isLoading && !shows && <div>No shows were Added</div>}
+    {!isLoading && !error && shows && <ShowGrid data={shows}/>}
+    </>
   )
 }
 
